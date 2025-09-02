8442e523e41ebc92c93e88d4075d43ac9a9993dd
AchiGel <ar.gelashvili@gmail.com>
2025-09-02 15:36:06 +0400

    prepared for the deployment

--- server/src/server.js
+++ server/src/server.js
@@ -3,16 +3,29 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use(cors());
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
